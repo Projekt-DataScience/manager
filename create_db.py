96e94a89959db95d6367fa23cf3cf8740154156d
Fabian Hueber <fabi_boy@hotmail.de>
2022-12-22 16:54:55 +0100

    Revert "see issue #3"
    This reverts commit 8fac6c05a67e44a6ec2da0c26981db6054bd9b21.

--- create_db.py
+++ create_db.py
@@ -1,23 +1,17 @@
 import os
 import time
 import random
-import sqlalchemy
 
 from backend_db_lib.models import base, LPAQuestion, LPAQuestionCategory
 from backend_db_lib.manager import DatabaseManager
 
-DATABASE_URL = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOSTNAME')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}"
-
-	
-db = DatabaseManager(base, DATABASE_URL)
-def database_is_empty():
-    table_names = sqlalchemy.inspect(db._db).get_table_names()
-    return len(table_names) == 0
-	
-if not database_is_empty():
+if os.path.exists("/root/db_created.txt"):
     print("DB already created")
     exit(0)
-	
+
+DATABASE_URL = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOSTNAME')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}"
+
+db = DatabaseManager(base, DATABASE_URL)
 db.drop_all()
 print("Tables dropped.")
 
@@ -28,4 +22,9 @@ CREATE_INITIAL_DATA = int(os.environ.get('CREATE_INITIAL_DATA', 0))
 print("CREATE_INITIAL_DATA", CREATE_INITIAL_DATA, type(CREATE_INITIAL_DATA))
 if CREATE_INITIAL_DATA == 1:
     db.create_initial_data()
+
     print("Data created.")
+
+
+with open("/root/db_created.txt", "w") as f:
+    f.write("1")
